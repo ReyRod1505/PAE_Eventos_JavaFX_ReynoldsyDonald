fe9fdc8783399ddf323b8246fd5a99d633f8940e
ReyRod1505 <reynoldsra@uamv.edu.ni>
2026-09-01 15:51:40 -0600

    Implementa registro y búsqueda de productos

---
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_reynoldsydonald/reto1/Reto1Application.java
@@ -0,0 +1,24 @@
+package ni.edu.uam.pae_eventos_javafx_reynoldsydonald.reto1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Reto1Application extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Reto1Application.class.getResource("reto1-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 480, 560);
+        stage.setTitle("Reto 1 - Inventario de Pulpería");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
